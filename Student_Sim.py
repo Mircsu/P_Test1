090ab4f114371e14803434e13f6d99ab28cf2d27
YevheniiDimov <dimovj12@gmail.com>
2023-05-13 14:47:16 +0300

    asdgfhdjfjdkd

--- Student_Sim.py
+++ Student_Sim.py
@@ -1,5 +1,5 @@
 import random
-    class Student :
+class Student :
         def __init__(self,name):
             self.name=name
             self.radist=1
@@ -27,3 +27,19 @@ import random
         def dayoff(self):
             print("Your happynes =", self.radist)
             print("Your progres =", self.progres)
+        def simulate(self,day):
+            day=day
+            rnd=random.randint(1,3)
+            if(rnd==1):
+                self.study()
+            if(rnd==2):
+                self,chill()
+            if(rnd==3):
+                self.sleep()
+                self.proverka()
+                self.dayoff()
+ivan=Student(name="Dima")
+for day in range(7):
+    ivan.simulate(day)
+
+
