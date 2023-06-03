137c9b0563cc8d4b05f5c1731131836058e924d5
YevheniiDimov <dimovj12@gmail.com>
2023-06-03 13:36:29 +0300

    asdgfhdjfjdkd

---
+++ 123.py
@@ -0,0 +1,27 @@
+class Human:
+    def __init__(self,name,age):
+        self.name=name
+        self.set_age=(age)
+    def get_age(self):
+        return self.__age
+    def set_age(self,age):
+        if age>0
+            self.__age=age
+        else:
+            print("Такого не буває")
+
+h=Human("Dima",-1)
+print(h.name, " Його вік = ", h.get_age() )
+
+class Student:
+    def __init__(self,Monday,Thuesday, Wednesday, Thursday, Friday, Saturday):
+        self.Monday=Monday
+        self.Thuesday=Thuesday
+        self.Wednesday=Wednesday
+        self.Thursday=Thursday
+        self.Friday=Friday
+        self.__Saturday=Saturday
+d=Student("1","2","3","4","5",6)
+d.Monday,Thuesday,Wednesday,Thursday,Friday="12345"
+d.Saturday=7
+print(d.)
