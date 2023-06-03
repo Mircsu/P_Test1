988149d786c564d16201b0fff8f082134e40e684
YevheniiDimov <dimovj12@gmail.com>
2023-06-03 15:37:32 +0300

    asdgfhdjfjdkd

---
+++ F_polimorphism.py
@@ -0,0 +1,20 @@
+class Banana:
+
+    def geo(self):
+        print("Africa")
+class Apple:
+    def geo(self):
+        print("Kazakhstan")
+
+class Potato:
+
+    def geo(self):
+        print("America")
+
+Apple().geo()
+Banana().geo()
+Potato().geo()
+
+
+
+
